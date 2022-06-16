1b3902705a105b0a65c2105a43664095e4163064
MiamiCenter <dawidow735@gmail.com>
2022-06-16 22:08:25 +0200

    Update interactionext.lua

--- InteractionCheck/interactionext.lua
+++ InteractionCheck/interactionext.lua
@@ -1,27 +1,27 @@
 	Hooks:RemovePostHook("check_interaction_obstacles")
 	local dir = Vector3()
 	local ignored_units = {
-		fish = {"Idstring(@ID43dd7534e20f2963@)", "Idstring(@ID165a3c90065493f6@)", "Idstring(@IDffedb8a6e44b1615@)"}, --fish (Yacht Heist) (harddrive, money bundles)
-		crojob2 = {"Idstring(@ID4e2b16fa53a8d1f3@)", "Idstring(@IDf1461ef41b1bce71@)", "Idstring(@ID7dc5f755ea3f7d65@)", "Idstring(@ID6964595ea36b2825@)", "Idstring(@ID18a8927023a02105@)", "Idstring(@IDac51bbdbcccdb234@)", "Idstring(@ID815dd21893ac11b5@)"}, --BombHeist (Meth ingridients, bomb parts)
-		dah = {"Idstring(@ID0727920b09b4bb19@)", "Idstring(@ID66943d8696d23a80@)", "Idstring(@ID1a7e58366dd21dbb@)", "Idstring(@ID2274fdfb1d14c18f@)", "Idstring(@ID36506b0e7109039a@)", "Idstring(@IDba78978ec6db4407@)", "Idstring(@ID4dd7012fe83e7c95@)", "Idstring(@IDc12bc6ddf09a7a56@)", "Idstring(@ID0c1ea5bfa95cb271@)"}, --Diamond Heist (Loot inside the vault)
-		mus = {"Idstring(@IDa163786a6ddb0291@)"}, --The Diamond (bodybag case)
-		jewelry_store = {"Idstring(@IDe8088e3bdae0ab9e@)", "Idstring(@ID05956ff396f3c58e@)"}, --Jewelry Store (gage_package)
-		kenaz = {"Idstring(@ID963b39ffe1ab38d5@)"}, --Golden Grin Casino (Sleeping gas)
-		sah = {"Idstring(@ID4989e002f366b351@)"}, --Schacklethorne Mansion (Black Plate)
-		ukrainian_job = {"Idstring(@IDe8088e3bdae0ab9e@)", "Idstring(@ID86a3676e0b178dda@)"}, --UkrainianJob (gage_package, jewelry)
-		dark = {"Idstring(@ID29c64eba7ea1bb4f@)", "Idstring(@IDffedb8a6e44b1615@)", "Idstring(@ID9c0e4f7e2193a163@)"}, --Murkystation (termite past, harddrive)
-		firestarter_1 = {"Idstring(@ID584bea03f3b5d712@)", "Idstring(@ID34d05ca941b5ebfe@)"}, --Firestarter day 1 (Truck drill and C4)
-		bex = {"Idstring(@ID9e05f9d8ca3ef529@)", "Idstring(@ID40c5b07b6561cc1f@)", "Idstring(@ID70cc9cf82e4c87a9@)"}, --San martin bank (Manager safe)
-		welcome_to_the_jungle_2 = {"Idstring(@ID641e583b23d2136a@)"}, --Big Oil day 2 jewellery
-		mex = {"Idstring(@ID9819a50244591ecf@)", "Idstring(@IDd90bf4a3a96e4d3b@)", "Idstring(@ID919af1f7f773ed60@)"}, --Crates and C4
-		nail = {"Idstring(@ID8fa207632e271d4b@)"}, --lab Rats pills
-		brb = {"Idstring(@ID75fac870cd2c4062@)", "Idstring(@ID22152b498be8fdbc@)"}, --Brooklyn Bank C4
-		hox_2 = {"Idstring(@IDe4b9625bac2807e5@)", "Idstring(@ID9c0e4f7e2193a163@)"}, --Hox Breakout Server and harddrive
-		vit = {"Idstring(@IDc06b518a90138065@)"}, --White House PEOC PC
-		pent = {"Idstring(@ID2286ce1b2545957b@)", "Idstring(@IDffedb8a6e44b1615@)", "Idstring(@IDb4d77ec3cc8189f3@)", "Idstring(@IDcc29feaebf2838f4@)"}, --Mountain Master (vent, harddrive)
-		flat = {"Idstring(@ID90b388e6fe8c6416@)"}, --Panic Room (Saws)
-		rat = {"Idstring(@ID8f59e19e1e45a05e@)", "Idstring(@ID43ed278b1faf89b3@)"}, --Cook off, Ammo Bag asset, Medic Bag
-		alex_3 = {"Idstring(@ID8f59e19e1e45a05e@)", "Idstring(@ID43ed278b1faf89b3@)"} --Cook off, Ammo Bag asset, Medic Bag
+		fish = {"Idstring(@ID43dd7534e20f2963@)", "Idstring(@ID165a3c90065493f6@)", "Idstring(@IDffedb8a6e44b1615@)"},
+		crojob2 = {"Idstring(@ID4e2b16fa53a8d1f3@)", "Idstring(@IDf1461ef41b1bce71@)", "Idstring(@ID7dc5f755ea3f7d65@)", "Idstring(@ID6964595ea36b2825@)", "Idstring(@ID18a8927023a02105@)", "Idstring(@IDac51bbdbcccdb234@)", "Idstring(@ID815dd21893ac11b5@)"},
+		dah = {"Idstring(@ID0727920b09b4bb19@)", "Idstring(@ID66943d8696d23a80@)", "Idstring(@ID1a7e58366dd21dbb@)", "Idstring(@ID2274fdfb1d14c18f@)", "Idstring(@ID36506b0e7109039a@)", "Idstring(@IDba78978ec6db4407@)", "Idstring(@ID4dd7012fe83e7c95@)", "Idstring(@IDc12bc6ddf09a7a56@)", "Idstring(@ID0c1ea5bfa95cb271@)"},
+		mus = {"Idstring(@IDa163786a6ddb0291@)"},
+		jewelry_store = {"Idstring(@IDe8088e3bdae0ab9e@)", "Idstring(@ID05956ff396f3c58e@)"},
+		kenaz = {"Idstring(@ID963b39ffe1ab38d5@)"},
+		sah = {"Idstring(@ID4989e002f366b351@)"},
+		ukrainian_job = {"Idstring(@IDe8088e3bdae0ab9e@)", "Idstring(@ID86a3676e0b178dda@)"},
+		dark = {"Idstring(@ID29c64eba7ea1bb4f@)", "Idstring(@IDffedb8a6e44b1615@)", "Idstring(@ID9c0e4f7e2193a163@)"},
+		firestarter_1 = {"Idstring(@ID584bea03f3b5d712@)", "Idstring(@ID34d05ca941b5ebfe@)"},
+		bex = {"Idstring(@ID9e05f9d8ca3ef529@)", "Idstring(@ID40c5b07b6561cc1f@)", "Idstring(@ID70cc9cf82e4c87a9@)"},
+		welcome_to_the_jungle_2 = {"Idstring(@ID641e583b23d2136a@)"},
+		mex = {"Idstring(@ID9819a50244591ecf@)", "Idstring(@IDd90bf4a3a96e4d3b@)", "Idstring(@ID919af1f7f773ed60@)"},
+		nail = {"Idstring(@ID8fa207632e271d4b@)"},
+		brb = {"Idstring(@ID75fac870cd2c4062@)", "Idstring(@ID22152b498be8fdbc@)"},
+		hox_2 = {"Idstring(@IDe4b9625bac2807e5@)", "Idstring(@ID9c0e4f7e2193a163@)"},
+		vit = {"Idstring(@IDc06b518a90138065@)"},
+		pent = {"Idstring(@ID2286ce1b2545957b@)", "Idstring(@IDffedb8a6e44b1615@)", "Idstring(@IDb4d77ec3cc8189f3@)", "Idstring(@IDcc29feaebf2838f4@)"},
+		flat = {"Idstring(@ID90b388e6fe8c6416@)"},
+		rat = {"Idstring(@ID8f59e19e1e45a05e@)", "Idstring(@ID43ed278b1faf89b3@)"},
+		alex_3 = {"Idstring(@ID8f59e19e1e45a05e@)", "Idstring(@ID43ed278b1faf89b3@)"}
 	}
 	local ignore_packages = {"jewelry_store", "ukrainian_job", "branchbank", "firestarter_3", "des"}
 	local last_printed_unit = "none"
@@ -55,45 +55,3 @@
 			return false
 		end
 	end)
---[[
-local units = World:find_units_quick("all")
-
-for _, unit in pairs(units) do
-	if alive(unit) and unit:interaction() and unit:interaction():active() then
-		local carry_data = unit:carry_data()
-		local weapon_case = unit:interaction().tweak_data == "weapon_case"
-		local pickup_id = unit:base() and unit:base().small_loot
-		local int_data = unit:interaction().tweak_data and tweak_data.interaction[unit:interaction().tweak_data]
-		local special = unit:interaction()._special_equipment or int_data and int_data.special_equipment_block
-		if carry_data or pickup_id or special or weapon_case and unit:unit_data().unit_id ~= 0 then
-			managers.hud:add_waypoint(unit:unit_data().unit_id, {
-				distance = true,
-				state = "sneak_present",
-				present_timer = 0,
-				text = "none",
-				icon = "pd2_bodybag",
-				position = unit:position()
-			})
-		end
-	end
-end
-
-Hooks:RemovePostHook("debug_find_units_wp_remove")
-Hooks:PostHook(ObjectInteractionManager, "remove_unit", "debug_find_units_wp_remove", function(self, unit)
-	if unit:unit_data() and unit:unit_data().unit_id and managers.hud:get_waypoint_data(unit:unit_data().unit_id) then
-		log("Removed ID = "..tostring(unit:unit_data().unit_id))
-		managers.hud:remove_waypoint(unit:unit_data().unit_id)
-	end
-end)
-
---Log unit ID that player is looking at
-	local player = managers.player:local_player()
-	local result
-	if player then 
-		result = player:movement():current_state()._fwd_ray
-	end
-	log(tostring(result.unit:unit_data().unit_id))
-
-local unit = managers.worlddefinition:get_unit(100496)
-managers.game_play_central:mission_disable_unit(unit)
-]]
